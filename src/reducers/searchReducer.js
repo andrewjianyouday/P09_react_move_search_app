58e3387038af3ccfc7f9befffdb0676eba70c57e
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-25 21:34:05 +0100

    return restult in search reducer based on action

--- src/reducers/searchReducer.js
+++ src/reducers/searchReducer.js
@@ -5,14 +5,39 @@
  * 
  */
 
+
+ /** 
+ * In SearchAction
+ * 1, user search with ' text ' 
+ * 2, this ' text ' will be passed to this SearchAction
+ * 3, this ' text ' will be set into the payload inside reducer (in searchReducer)
+ * 
+ * In SearchReducer
+ * 1, check action type if has same Type.
+ * 2, return  the text state into action payload which is text.
+ * 
+ */
  import {SEARCH_MOVIE} from '../action/types';
 
  // 4 basic states
- const initialStates = {
+ const initialState = {
      text = '', // user search text
      movies = [], // search result 
      loading: false, // f/t depending on data arrived/or not
      movie:[]
  }
 
- 
+ export default function(state = initialState, action){
+
+    switch(action.type){
+        case SEARCH_MOVIE:
+            return {
+                ...state,
+                text: action.payload,
+                loading: false
+            }
+        default: 
+        return state
+    }
+
+ }
