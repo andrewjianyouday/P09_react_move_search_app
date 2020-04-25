08bc1c1d8d2c10013240205e24cb99af8d762e7b
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-25 21:07:31 +0100

    wrape Navbar & Fotter in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 class App extends Component {
   render(){
     return(
-      <Navbar/>
+      <div>
+        <Navbar/>
+        <Footer/>
+      </div>
     );
   }
 }
