53bed340edd9d574e0b1c8f9ed7f3af9285ff7ca
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-26 22:51:21 +0100

    add Landing container with searchForm

---
+++ src/components/home/Landing.js
@@ -0,0 +1,17 @@
+/**
+ * container for the search bar
+ */
+import React, { Component } from 'react'
+import SearchForm from './SearchForm'
+
+export class Landing extends Component {
+    render() {
+        return (
+            <div className="container">
+               <SearchForm/> 
+            </div>
+        )
+    }
+}
+
+export default Landing
