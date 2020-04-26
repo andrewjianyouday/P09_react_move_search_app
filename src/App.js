1a00b449b5a443197e0bbfc74e22cff71f7d8889
andrewjianyouday <andrewjianyouday@gmail.com>
2020-04-26 22:50:25 +0100

    add Provider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import {Provider} from 'react-redux';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/home/Landing';
+import store from './store';
+
 
 class App extends Component {
   render(){
     return(
-      <div>
+      <Provider store = {store} >
         <Navbar/>
+        <Landing/>
         <Footer/>
-      </div>
+      </Provider>
     );
   }
 }
