0b040d135af60be686cdb2a171f819e69abd2850
andrewjianyouday <andrewjianyouday@gmail.com>
2020-05-16 08:52:32 +0100

    change <a  to  <Link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 // shortcut ' rcep' 
 // rcc -> create class based component...
@@ -12,9 +13,9 @@ function Navbar() {
             <nav className="navbar navbar-light bg-dark mb-5">
                 <div className="container">
                     <div className="navbar-header">
-                        <a className="navbar-brand text-white text-lg brand-text" to="/">
+                        <Link className="navbar-brand text-white text-lg brand-text" to="/">
                             MovieSeriesInfo
-                        </a>
+                        </Link>
                     </div>
                     <ul className="navbar-nav ml-auto text-light d-inline-block">
                         <li className="nav-item d-inline-block mr-4">
