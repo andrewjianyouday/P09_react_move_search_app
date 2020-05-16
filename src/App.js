6c63427f3363b5c94dabb6438f438960c89f2362
andrewjianyouday <andrewjianyouday@gmail.com>
2020-05-16 08:54:11 +0100

    fix bug  -> remove {} for impt movie component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
-import {Provider} from 'react-redux';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/home/Landing';
+import Movie from './components/home/Movie';
+
 import store from './store';
-import {HashRouter as Router} from 'react-router-dom'
+
+
 
 
 class App extends Component {
   render(){
     return(
-      <Provider store = {store} >
+      <Provider store={store} >
         <Router>
           <div>
             <Navbar/>
-            <Landing/>
+            <Route exact  path="/" component={Landing} />
+            <Route exact  path="/movie/:id" component={Movie} />
             <Footer/>
           </div>
         </Router>
